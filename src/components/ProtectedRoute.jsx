c78fb1b62971fd845c175c0ca933b3ac02248bd0
Singidunum64 <152324937+Singidunum64@users.noreply.github.com>
2026-01-15 17:31:36 +0100

    Minor tweaks

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,12 +4,10 @@ import { useAuth } from "../context/AuthContext";
 const ProtectedRoute = ({ children, role }) => {
   const { user } = useAuth();
 
-  // nije ulogovan
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  // ulogovan, ali nema odgovarajuću rolu
   if (role && user.role !== role) {
     return <Navigate to="/" replace />;
   }
